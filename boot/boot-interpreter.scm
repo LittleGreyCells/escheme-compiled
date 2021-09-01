9869904603332004a4d6e364d116206bfa44d46b
Larry Jordan <larry.jordan@cox.net>
2021-09-01 08:35:52 -0700

    prep for stklos module rollout

--- boot/boot-interpreter.scm
+++ boot/boot-interpreter.scm
@@ -6,6 +6,5 @@
 (load (system-path "macros/macros-interpreter.scm"))
 (load (system-path "boot/standard-macros.scm"))
 (load (system-path "boot/catch-throw.scm"))
-;; (load (system-path "boot/modules.scm"))
 
 ;; [EOF]
